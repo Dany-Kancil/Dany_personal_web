c39925c8532b01ae70ef7371c0553a1e1704e62c
Dany-Kancil <buyuli103@gmail.com>
2025-10-02 13:48:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const tombolSapa = document.getElementById('sapaButton');
-tombolSapa.addEventListener('click',function(){
-  alert('Halo! Terima kasih sudah berkunjung!');
+tombolSapa.addEventListener('click', function(){
+  //alert('Halo! Terima kasih sudah berkunjung!');
+  Swal.fire({
+    title: 'Halo!',
+    text: 'Terima kasih sudah berkunjung!',
+    icon: 'success', // 'success', 'error', 'warning', 'info', 'question'
+    confirmButtonText: 'OK'
+  });
 });
